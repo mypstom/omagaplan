1572f928f6800237b9f37ba646f73de3dd78c9bc
jienhuagoo <jienhuagoo@gujianhuas-MacBook-Pro.local>
2018-11-17 22:29:00 +0800

    * add ignore

---
+++ omegaplanserver/main.js
@@ -0,0 +1,44 @@
+var firebase = require("firebase");
+var express = require('express');
+var app = express();
+ 
+var config = {
+    apiKey: "AIzaSyDxgOeH5kzft0JQ6aSlzzSatdOff9sMmso",
+    authDomain: "ooaccountingsystem.firebaseapp.com",
+    databaseURL: "https://ooaccountingsystem.firebaseio.com",
+    projectId: "ooaccountingsystem",
+    storageBucket: "ooaccountingsystem.appspot.com",
+    messagingSenderId: "339874112428"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
+var ref = firebase.database().ref('status/');
+
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+app.get('/data', function (req, res) {
+    // 输出 JSON 格式
+    var response = {
+        "stockState":req.query.stockState,
+        "openState":req.query.openState,
+        "waterflow":req.query.waterflow
+    };
+    ref.set(response);
+    res.send("Receive date: " + JSON.stringify(response));
+ })
+ 
+var server = app.listen(process.env.PORT||3000, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("Server host on %s port", port)
+ 
+})
